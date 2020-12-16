e8c2375b1a7121da8020bea4e6c99e9dc8c601e0
LeSharsky <74248826+LeSharsky@users.noreply.github.com>
2020-12-16 15:33:54 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+files {
+    'index.html',
+    'css/style.css',
+    'js/script.js',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
